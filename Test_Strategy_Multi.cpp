3507b2b4ce844bbebef3161e8691023c1d8bba95
fsygd <fsygd1996@163.com>
2016-04-30 16:31:58 +0800

    Test_Strategy_Multi

---
+++ Test_Strategy_Multi.cpp
@@ -0,0 +1,30 @@
+#include "Test_Strategy_Multi.h"
+bool Test_Strategy_Multi::Test_Word(std::string Temp_Word){
+	int Temp1, Temp2, answer;
+	Temp1 = rand()%4+1;
+	std::cout << Temp_Word << std::endl;
+	for (int i = 1; i <= 4; ++i){
+		std::cout << (char)('A'-1+i) << " ";
+		if (i == Temp1) std::cout << data->Get_Chinese(Temp_Word);
+		else{
+			while (1){
+				Temp2 = rand()%data->Words.size();
+				if (data->Get_English(Temp2) != Temp_Word) break;
+			}
+			std::cout << data->Get_Chinese(data->Get_English(Temp2));
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "Press A or B or C or D" << std::endl;
+	char Temp[200];
+	while (1){
+		std::cin.getline(Temp, 200);
+		if (strlen(Temp) == 1){
+			if (Temp[0] == 'A') { answer = 1; break; }
+			if (Temp[0] == 'B') { answer = 2; break; }
+			if (Temp[0] == 'C') { answer = 3; break; }
+			if (Temp[0] == 'D') { answer = 4; break; }
+		}
+	}
+	return (answer == Temp1);
+}
---
+++ Test_Strategy_Multi.h
@@ -0,0 +1,4 @@
+#include "Test_Strategy.h"
+class Test_Strategy_Multi: public Test_Strategy{
+	bool Test_Word(std::string Temp_word);
+};
